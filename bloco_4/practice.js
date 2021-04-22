bb42c7516ed31e796bfa1026efeed37888dbe0fd
Jossany Moura <mjossany@gmail.com>
2021-04-22 12:10:34 -0300

    Adiciona practice.js e practice.html

---
+++ bloco_4/practice.js
@@ -0,0 +1,17 @@
+let status = 'reprovado';
+
+switch (status) {
+    case 'aprovado':
+        console.log('Parabéns, você foi aprovado');
+        break;
+
+    case 'lista':
+        console.log('Você está na lista de espera');
+        break;
+    case 'reprovado':
+        console.log('Você foi reprovado');
+        break;
+}
+
+
+
