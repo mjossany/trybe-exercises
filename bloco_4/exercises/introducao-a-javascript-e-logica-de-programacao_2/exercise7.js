576a02b3f3bd44e06a84504992cfe567f4de7ee0
Jossany Moura <mjossany@gmail.com>
2021-04-23 11:15:16 -0300

    Adiciona e resolve exercise7.js

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise7.js
@@ -0,0 +1,20 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//7. Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+//First I have to find de highest value of the array and than encounter the smallest
+
+let higherValue = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > higherValue) {
+        higherValue = numbers[index];
+    }
+}
+
+let smallestValue = higherValue;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < smallestValue) {
+        smallestValue = numbers[index];
+    }
+}
+console.log(smallestValue);
