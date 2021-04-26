bd62421d84de5b17fa7222f1d528ce0a09ddc443
Jossany Moura <mjossany@gmail.com>
2021-04-26 11:16:17 -0300

    Adiciona e resolve exercise3.js

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise3.js
@@ -0,0 +1,14 @@
+//3. Agora inverta o lado do triângulo.Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+
+let heigth = 5;
+
+for (let line = 0; line < heigth; line += 1){
+    let asteriskLine = '';
+    for (let blanks = 1; blanks < heigth - line; blanks += 1) {
+        asteriskLine += ' ';
+    }
+    for (let asterisks = 0; asterisks <= line; asterisks += 1) {
+        asteriskLine += '*';
+    }
+    console.log(asteriskLine);
+}
