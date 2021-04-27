98af2329eaa6028e5808de2812ec59424208302b
Jossany Moura <mjossany@gmail.com>
2021-04-26 22:07:14 -0300

    Adiciona resolucao ao exercise3.js

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise2.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise2.js
@@ -1,16 +1,16 @@
 //2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
-// let n = 5
+let n = 5
 
-// for (let lines = 0; lines < n; lines += 1) {   
-//     let asteriskLine = '';
-//     for (let columns = 0; columns <= lines; columns += 1) {
-//         asteriskLine += '*'
-//     }
-//     console.log(asteriskLine);
-// }
+for (let lines = 0; lines < n; lines += 1) {   
+    let asteriskLine = '';
+    for (let columns = 0; columns <= lines; columns += 1) {
+        asteriskLine += '*'
+    }
+    console.log(asteriskLine);
+}
 
-// or
+or
 
 let size = 5;
 let symbol = '*';
--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise3.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise3.js
@@ -1,14 +1,34 @@
 //3. Agora inverta o lado do triângulo.Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
 
-let heigth = 5;
+// let heigth = 5;
 
-for (let line = 0; line < heigth; line += 1) {
-    let asteriskLine = '';
-    for (let blanks = 1; blanks < heigth - line; blanks += 1) {
-        asteriskLine += ' ';
-    };
-    for (let asterisks = 0; asterisks <= line; asterisks += 1) {
-        asteriskLine += '*';
-    };
-    console.log(asteriskLine);
-};
+// for (let line = 0; line < heigth; line += 1) {
+//     let asteriskLine = '';
+//     for (let blanks = 1; blanks < heigth - line; blanks += 1) {
+//         asteriskLine += ' ';
+//     };
+//     for (let asterisks = 0; asterisks <= line; asterisks += 1) {
+//         asteriskLine += '*';
+//     };
+//     console.log(asteriskLine);
+// };
+
+// or
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n - 1;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine += ' ';
+    } else {
+      inputLine += symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+}
