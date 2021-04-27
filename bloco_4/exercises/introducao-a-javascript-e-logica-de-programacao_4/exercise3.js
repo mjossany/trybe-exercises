9066d1801a3364c578b51ff4291ef27bf720eeaf
Jossany Moura <mjossany@gmail.com>
2021-04-27 13:18:52 -0300

    Corrige exercise2.js e adiciona e resolve exercise3.js

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise3.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise3.js
@@ -1,4 +1,4 @@
-//2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+//3. Faça um for/in que mostre todas as chaves do objeto.
 
 let info = {
   personagem: 'Margarida',
@@ -8,4 +8,6 @@ let info = {
 
 info['recorrente'] = 'Sim';
 
-console.log(info);
+for (let key in info) {
+  console.log(key);
+};
