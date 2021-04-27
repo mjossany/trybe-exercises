4d7cfc0a8546fbf57238a1220f750f28d5734c9a
Jossany Moura <mjossany@gmail.com>
2021-04-27 08:30:14 -0300

    .

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise6.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise6.js
@@ -1,13 +1,12 @@
 //6. Faça um programa que diz se um número definido numa variável é primo ou não.
 
-let deposit = [];
-let isThisNumberPrime = 7;
-let divisors = 0;
+let isThisNumberPrime = 5;
 
+let divisors = 0;
 for (let index = 2; index <= isThisNumberPrime; index += 1) {
-    if (isThisNumberPrime % index === 0) {
-        divisors += 1;
-    }
+  if (isThisNumberPrime % index === 0) {
+      divisors += 1;
+  }
 }
 if (divisors > 1) {
     console.log(`${isThisNumberPrime} não é primo`);
