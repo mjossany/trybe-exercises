e2a0064c5be99469cff916fa89c62d0b757df642
Jossany Moura <mjossany@gmail.com>
2021-04-28 00:48:18 -0300

    Resolve FuncoesBonusI.js

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_4/FuncoesBonusI.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_4/FuncoesBonusI.js
@@ -2,29 +2,33 @@
 // Atenção! Esse exercício já apareceu no processo seletivo de uma grande multinacional!
 // Dicas: Uma string é um array de caracteres, então cada elemento do array é uma letra.
 
-let numeroRomano = 'XIV';
-let romanos = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000,
-};
-const converteRomanosEmInteiro = (numeroRomano) => {
-  let arrayInteiros = [];
-  for (let index = 0; index < numeroRomano.length; index += 1 ) {
-    arrayInteiros.push(romanos[numeroRomano[index]]);
+function Romanos(roma) {
+  let romanos = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
   };
-  let soma = arrayInteiros[arrayInteiros.length - 1];
-  for (let arrayIndex = arrayInteiros.length - 2; arrayIndex >= 0; arrayIndex -= 1) {
-    if (soma > arrayInteiros[arrayIndex]) {
-      soma -= arrayInteiros[arrayIndex];
+
+  let arrayNumbers = [];
+  let result = 0;
+
+  for (let indice in roma) {
+    arrayNumbers[indice] = romanos[roma[indice]];
+  }
+
+  for (let indice = 0; indice < arrayNumbers.length; indice += 1) {
+    if (arrayNumbers[indice] < arrayNumbers[indice + 1]) {
+      arrayNumbers[indice + 1] = arrayNumbers[indice + 1] - arrayNumbers[indice];
     } else {
-      soma += arrayInteiros[arrayIndex];
-    };
-  };
-  return soma;
-};
-console.log(converteRomanosEmInteiro(numeroRomano));
+      result += arrayNumbers[indice];
+    }
+  }
+
+  return result;
+}
+
+console.log(Romanos('XIV'));
