5392beb0ce17179876acd7500a8524a07b4c4ad9
Jossany Moura <mjossany@gmail.com>
2021-04-29 08:38:53 -0300

    Adiciona practice.html e script.js

---
+++ bloco_5/dia1/script.js
@@ -0,0 +1,7 @@
+document.getElementById('page-title').innerText = 'Shawshank Redemption';
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+let secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.innerText = 'Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the mans unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.';
+let subtitle = document.getElementById('subtitle');
+subtitle.innerText = 'Storyline';
