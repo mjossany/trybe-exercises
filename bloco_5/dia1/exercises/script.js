bf053f97ddcd30ec9ed5a635783d70552126f45d
Jossany Moura <mjossany@gmail.com>
2021-04-29 11:31:39 -0300

    Finaliza todas tarefas menos a 5

--- bloco_5/dia1/exercises/script.js
+++ bloco_5/dia1/exercises/script.js
@@ -1,3 +1,9 @@
 document.getElementsByTagName('p')[1].innerText = 'Daqui a 2 anos me vejo como um programador em evolução dentro de uma empresa de tecnologia';
 document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
 document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
+document.getElementsByClassName('title')[0].innerText = 'Exercício 5.1 - JavaScript';
+let p = document.getElementsByTagName('p');
+for (let index = 0; index < p.length; index += 1) {
+  p[index] = p[index].toUpperCase();
+  console.log(p[index]);
+}
