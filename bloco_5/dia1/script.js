b0bbe2bbf1e8cb686cabd70c8891f832bc04406b
Jossany Moura <mjossany@gmail.com>
2021-04-29 10:45:51 -0300

    Altera practice.html e script.js

--- bloco_5/dia1/script.js
+++ bloco_5/dia1/script.js
@@ -1,7 +1,10 @@
-document.querySelector('#page-title').innerText = 'Shawshank Redemption';
-let paragraphs = document.querySelectorAll('.paragraphs');
-paragraphs[0].style.color = 'red';
-paragraphs[1].innerText = 'Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the mans unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.';
-let subtitle = document.querySelector('h4');
-subtitle.innerText = 'Storyline';
-subtitle.style.color = 'blue';
+
+
+document.getElementById('header-container').style.backgroundColor = 'rgb(0, 176, 105)';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'salmon';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'rgb(249, 219, 94)';
+let h3s = document.getElementsByTagName('h3');
+for (let index = 0; index < h3s.length; index += 1) {
+  h3s[index].style.backgroundColor = 'black';
+}
+document.getElementById('footer-container').style.backgroundColor = 'rgb(0, 53, 51)';
