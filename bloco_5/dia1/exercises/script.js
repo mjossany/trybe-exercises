45664d6fe426497b09fb180c67fd9d61250a7679
Jossany Moura <mjossany@gmail.com>
2021-04-29 11:10:40 -0300

    Finaliza tareda 2 e 3

--- bloco_5/dia1/exercises/script.js
+++ bloco_5/dia1/exercises/script.js
@@ -1,1 +1,3 @@
 document.getElementsByTagName('p')[1].innerText = 'Daqui a 2 anos me vejo como um programador em evolução dentro de uma empresa de tecnologia';
+document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
+document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
