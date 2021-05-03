a2807d2a7a685e14270ab56c04b0842f50728573
Jossany Moura <mjossany@gmail.com>
2021-05-03 20:45:49 -0300

    Resolve exercício7

--- bloco_5/dia3/exercises/script.js
+++ bloco_5/dia3/exercises/script.js
@@ -90,3 +90,44 @@ btnSexta.addEventListener ('click', function () {
     }
   }
 });
+
+//Exercício 6
+
+// function dayMouseOver() {
+//   let days = document.querySelector('#days');
+
+//   days.addEventListener('mouseover', function(event) {
+//     event.target.style.fontSize = '30px';
+//     event.target.style.fontWeight = '600';
+//   })
+// };
+
+// function dayMouseOut() {
+//   let days = document.querySelector('#days');
+
+//   days.addEventListener('mouseout', function(event) {
+//     event.target.style.fontWeight = '200';
+//     event.target.style.fontSize = '20px';
+//   })
+// };
+
+// dayMouseOver();
+// dayMouseOut();
+function dayMouseOver() {
+  const monthDays = document.querySelector('#days');
+  monthDays.addEventListener('mouseover', (event) => {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600';
+  });
+};
+
+function dayMouseOut() {
+  const monthDays = document.querySelector('#days');
+  monthDays.addEventListener('mouseout', (event) => {
+    event.target.style.fontSize = '20px';
+    event.target.style.fontWeight = '200';
+  });
+}
+
+dayMouseOver();
+dayMouseOut();
