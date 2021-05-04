3bb36ebad8017875522fdaa2b08e712d07ba0254
Jossany Moura <mjossany@gmail.com>
2021-05-04 11:15:29 -0300

    Resolve Exercício bônus

--- bloco_5/dia3/exercises/script.js
+++ bloco_5/dia3/exercises/script.js
@@ -151,3 +151,33 @@ for (let index = 0; index < addEvent.length; index += 1) {
     }
   })
 };
+
+//Exercício Bônus
+function addNewTask() {
+  let getInputField = document.querySelector('#task-input');
+  let addInputButton = document.querySelector('#btn-add');
+  let getTaskList = document.querySelector('.task-list');
+
+  addInputButton.addEventListener('click', function() {
+    if (getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    } else {
+      alert('Error: Digite ao menos 1 caractere.');
+    }
+  })
+
+  getInputField.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13 && getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    }
+  })
+}
+addNewTask();
