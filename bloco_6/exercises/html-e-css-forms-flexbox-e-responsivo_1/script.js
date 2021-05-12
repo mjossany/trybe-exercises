34bd8c4998b887f15664f96a2dbd266da9e81e0e
Jossany Moura <mjossany@gmail.com>
2021-05-12 18:42:24 -0300

    finaliza formulário

--- bloco_6/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ bloco_6/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,3 +1,5 @@
+let erro = false;
+
 function preencheSelect() {
   const catchSelect = document.getElementById('state');
   const siglasArray = ["ac", "al", "am", "ap", "ba", "ce", "df", "es", "go", "ma", "mt", "ms", "mg", "pa", "pb", "pr", "pe", "pi", "rj", "rn", "ro", "rs", "rr", "sc", "se", "sp", "to"];
@@ -7,23 +9,24 @@ function preencheSelect() {
     const newOption = document.createElement('option');
     newOption.value = siglasArray[index];
     newOption.innerHTML = estadosArray[index];
-
     catchSelect.appendChild(newOption);
   }
 }
 preencheSelect();
 
+const catchDataInput = document.getElementById('input-datainicio');
 function dateValidation() {
-  const catchDataInput = document.getElementById('input-datainicio');
   if(catchDataInput.value.length === 0) {
-    alert('A data não foi preenchida!');
+    erro = true;
+    catchDivFinal.innerHTML = 'A data não foi preenchida!';
     return false;
   }
 
   let regex = /\d\d\/\d\d\/\d\d\d\d/;
 
   if(!regex.test(catchDataInput.value)) {
-    alert('A data deve conter o formato dd/mm/aaaa');
+    erro = true;
+    catchDivFinal.innerHTML = 'A data deve conter o formato dd/mm/aaaa';
     return false;
   }
 
@@ -33,129 +36,151 @@ function dateValidation() {
   let year = splitted[3];
 
   if(day < 0 || day > 30) {
-    alert('Dia inválido');
+    erro = 1;
+    erro = true;
+    catchDivFinal.innerHTML = 'Dia inválido';
     return false;
   }
 
   if(month < 1 || month > 12) {
-    alert('Mês inválido');
+    erro = true;
+    catchDivFinal.innerHTML = 'Mês inválido';
     return false;
   }
 
   if(year < 0) {
-    alert('Ano inválido');
+    erro = true;
+    catchDivFinal.innerHTML = 'Ano inválido';
     return false;
   }
   return true;
 }
 
+const catchDivFinal = document.getElementById('final-message');
+const catchNameInput = document.getElementById('name');
 function nameValidation() {
-  const catchNameInput = document.getElementById('name');
   if(catchNameInput.value.length === 0) {
-    alert('O nome não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML = 'O nome não foi preenchido';
     return false;
   }
   if(catchNameInput.value.length > 40) {
-    alert('Preenchimento do campo Nome não pode ultrapassar 40 caracteres');
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo Nome não pode ultrapassar 40 caracteres';
     return false;
   }
   return true;
 }
 
+const catchEmailInput = document.getElementById('email');
 function emailValidation() {
-  const catchEmailInput = document.getElementById('email');
   if(catchEmailInput.value.length === 0) {
-    alert('O email não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML = 'O email não foi preenchido';
     return false;
   }
   if(catchEmailInput.value.length > 50) {
-    alert('Preenchimento do campo Email não pode ultrapassar 50 caracteres');
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo Email não pode ultrapassar 50 caracteres';
     return false;
   }
   return true;
 }
 
+const catchCpfInput = document.getElementById('cpf-number');
 function cpfValidation() {
-  const catchCpfInput = document.getElementById('cpf-number');
   if(catchCpfInput.value.length === 0) {
-    alert('O cpf não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML ='O cpf não foi preenchido';
     return false;
   }
   if(catchCpfInput.value.length > 11) {
-    alert('Preenchimento do campo CPF não pode ultrapassar 11 caracteres');
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo CPF não pode ultrapassar 11 caracteres';
     return false;
   }
   return true;
 }
 
+const catchEnderecoInput = document.getElementById('address');
 function enderecoValidation() {
-  const catchEnderecoInput = document.getElementById('address');
   if(catchEnderecoInput.value.length === 0) {
-    alert('O endereço não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML = 'O endereço não foi preenchido';
     return false;
   }
   if(catchEnderecoInput.value.length > 200) {
-    alert('Preenchimento do campo Endereço não pode ultrapassar 200 caracteres');
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo Endereço não pode ultrapassar 200 caracteres';
     return false;
   }
   return true;
 }
 
+const catchCidadeInput = document.getElementById('city');
 function cidadeValidation() {
-  const catchCidadeInput = document.getElementById('city');
   if(catchCidadeInput.value.length === 0) {
-    alert('O campo Cidade não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML = 'O campo Cidade não foi preenchido';
     return false;
   }
   if(catchCidadeInput.value.length > 200) {
-    alert('Preenchimento do campo Cidade não pode ultrapassar 200 caracteres');
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo Cidade não pode ultrapassar 200 caracteres';
     return false;
   }
   return true;
 }
 
+const catchEstadoInput = document.getElementById('state');
 function estadoValidation() {
-  const catchEstadoInput = document.getElementById('state');
   if(catchEstadoInput.value.length === 0) {
-    alert('O campo Estado não foi preenchido. Selecione um dos estados da lista.');
+    erro = true;
+    catchDivFinal.innerHTML = 'O campo Estado não foi preenchido. Selecione um dos estados da lista.';
     return false;
   }
 }
 
+const catchCurriculoInput = document.getElementById('resume');
 function curriculoValidation() {
-  const catchCurriculoInput = document.getElementById('resume');
   if(catchCurriculoInput.value.length === 0) {
-    alert('O campo Currículo não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML = 'O campo Currículo não foi preenchido';
     return false;
   }
   if(catchCurriculoInput.value.length > 1000) {
-    alert('Preenchimento do campo Resumo do currículo não pode ultrapassar 1000 caracteres');
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo Resumo do currículo não pode ultrapassar 1000 caracteres';
     return false;
   }
   return true;
 }
 
+const catchCargoInput = document.getElementById('role');
 function cargoValidation() {
-  const catchCargoInput = document.getElementById('role');
   if(catchCargoInput.value.length === 0) {
-    alert('O campo Cargo não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML = 'O campo Cargo não foi preenchido';
     return false;
   }
   if(catchCargoInput.value.length > 40) {
-    alert('Preenchimento do campo Cargo não pode ultrapassar 40 caracteres');
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo Cargo não pode ultrapassar 40 caracteres';
     return false;
   }
   return true;
 }
 
+const catchDescricaoInput = document.getElementById('roleDescription');
 function catchDescricaoValidation() {
-  const catchDescricaoInput = document.getElementById('roleDescription');
   if(catchDescricaoInput.value.length === 0) {
-    alert('O campo Descrição do cargo não foi preenchido');
+    erro = true;
+    catchDivFinal.innerHTML = 'O campo Descrição do cargo não foi preenchido';
     return false;
   }
-  if(catchCargoInput.value.length > 500) {
-    alert('Preenchimento do campo Descrição do cargo não pode ultrapassar 500 caracteres');
+  if(catchDescricaoInput.value.length > 500) {
+    erro = true;
+    catchDivFinal.innerHTML = 'Preenchimento do campo Descrição do cargo não pode ultrapassar 500 caracteres';
     return false;
   }
   return true;
@@ -166,7 +191,7 @@ catchSubmitButton.addEventListener('click', validateAll);
 
 function validateAll(event) {
   event.preventDefault();
-
+  erro = false;
   nameValidation();
   emailValidation();
   cpfValidation();
@@ -177,4 +202,21 @@ function validateAll(event) {
   curriculoValidation();
   cargoValidation();
   catchDescricaoValidation();
+  validationError();
 }
+
+const catchFormInput = document.getElementById('cv-form');
+function validationError() {
+  if(!erro) {
+    catchDivFinal.innerHTML = `${catchDataInput.value}
+    ${catchCpfInput.value} 
+    ${catchEnderecoInput.value} 
+    ${catchCidadeInput.value} 
+    ${catchEstadoInput.value} 
+    ${catchCurriculoInput.value}
+    ${catchCargoInput.value}
+    ${catchDescricaoInput.value} 
+    ${catchDataInput.value}`;
+  }
+}
+
