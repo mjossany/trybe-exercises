a3cf43ebd282f47f01606967c02eb8d3ed54b924
Jossany Moura <mjossany@gmail.com>
2021-05-13 11:03:33 -0300

    Finaliza validacoes

--- bloco_6/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ bloco_6/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -19,6 +19,27 @@ var picker = new Pikaday({
   format: 'DD/MM/YYYY'
 });
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
 // const catchDataInput = document.getElementById('input-datainicio');
 // function dateValidation() {
 //   if(catchDataInput.value.length === 0) {
