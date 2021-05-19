5c3136a98998453e9a7c30a29b8717ec04f25c54
Jossany Moura <mjossany@gmail.com>
2021-05-19 16:00:26 -0300

    Resolve parte I de movo diferente

--- bloco_7/7.1/parteI.js
+++ bloco_7/7.1/parteI.js
@@ -11,18 +11,26 @@
 
 // testingScope(true);
 
+// let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// // Seu código aqui.
+// const ordemCrescente = (arrayOfNumbers) => {
+//   for (let index = 1; index < arrayOfNumbers.length; index += 1) {
+//     for (let indexII = 0; indexII < index; indexII += 1) {
+//       if (arrayOfNumbers[index] < arrayOfNumbers[indexII]) {
+//         let save = arrayOfNumbers[indexII];
+//         arrayOfNumbers[indexII] = arrayOfNumbers[index];
+//         arrayOfNumbers[index] = save;
+//       }
+//     }
+//   }
+//   console.log(`Os números ${arrayOfNumbers} se encontram ordenados de forma crescente!`);
+// }
+// ordemCrescente(oddsAndEvens);
+
 let oddsAndEvens = [13, 3, 4, 10, 7, 2];
-// Seu código aqui.
+//Seu código aqui
 const ordemCrescente = (arrayOfNumbers) => {
-  for (let index = 1; index < arrayOfNumbers.length; index += 1) {
-    for (let indexII = 0; indexII < index; indexII += 1) {
-      if (arrayOfNumbers[index] < arrayOfNumbers[indexII]) {
-        let save = arrayOfNumbers[indexII];
-        arrayOfNumbers[indexII] = arrayOfNumbers[index];
-        arrayOfNumbers[index] = save;
-      }
-    }
-  }
+  arrayOfNumbers.sort(function(a, b){return a-b});
   console.log(`Os números ${arrayOfNumbers} se encontram ordenados de forma crescente!`);
 }
 ordemCrescente(oddsAndEvens);
