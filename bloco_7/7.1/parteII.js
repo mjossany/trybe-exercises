d720365c0f83d15b34ed9d58264c96468cc1f6e0
Jossany Moura <mjossany@gmail.com>
2021-05-19 17:40:03 -0300

    Resolve 1 parte II com recursividade

--- bloco_7/7.1/parteII.js
+++ bloco_7/7.1/parteII.js
@@ -1,10 +1,15 @@
-const factorial = number => {
-  let numberFactorial = 1;
-  if (number > 0) {
-    for (let index = 2; index <= number; index += 1) {
-      numberFactorial *= index;
-    }
-  }
-  console.log(numberFactorial)
-}
-factorial(6);
+// const factorial = number => {
+//   let numberFactorial = 1;
+//   if (number > 0) {
+//     for (let index = 2; index <= number; index += 1) {
+//       numberFactorial *= index;
+//     }
+//     console.log(numberFactorial)
+//   } else if (number === 0) {
+//     console.log('1');
+//   }
+// }
+// factorial(0);
+
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+console.log(factorial(5));
