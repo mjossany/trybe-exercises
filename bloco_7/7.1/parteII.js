48cc885967d787e9b23635e08a42d785f73651db
Jossany Moura <mjossany@gmail.com>
2021-05-19 18:02:04 -0300

    Resolve 2 parte II

--- bloco_7/7.1/parteII.js
+++ bloco_7/7.1/parteII.js
@@ -1,3 +1,4 @@
+//Questão 1
 // const factorial = number => {
 //   let numberFactorial = 1;
 //   if (number > 0) {
@@ -11,5 +12,23 @@
 // }
 // factorial(0);
 
-const factorial = number => number > 1 ? number * factorial(number - 1) : 1
-console.log(factorial(5));
+// Resolvendo por recursividade
+// const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+// console.log(factorial(5));
+
+//Questão 2
+
+const longestWord = string => {
+  const stringArray = string.split(" ");
+  let maxLength = 0;
+  let maxLengthWord = '';
+
+  for (let index = 0; index < stringArray.length; index += 1) {
+    if (stringArray[index].length > maxLength) {
+      maxLength = stringArray[index].length;
+      maxLengthWord = stringArray[index];
+    }
+  }
+  console.log(`A maior palavra da string é "${maxLengthWord}".`);
+}
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu");
