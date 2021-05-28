3f93a6365290d72a3f5f2369d554e4e1c47b1f68
Jossany Moura <mjossany@gmail.com>
2021-05-28 18:21:53 -0300

    Finaliza exercise4.js e exercise5.js

--- bloco_8/dia4/exercises/exercise4.js
+++ bloco_8/dia4/exercises/exercise4.js
@@ -2,3 +2,9 @@
 const assert = require('assert');
 const books = require('./books');
 
+function longestNamedBook() {
+  // escreva seu código aqui
+  return books.reduce((acc, book) => book.name.length > acc.name.length ? acc === book : acc).name
+}
+console.log(longestNamedBook());
+// assert.deepStrictEqual(longestNamedBook(), expectedResult);
