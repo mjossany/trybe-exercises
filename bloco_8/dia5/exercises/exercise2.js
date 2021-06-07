31f568b21f6ef56c85f585e2073f0426e7fc8698
Jossany Moura <mjossany@gmail.com>
2021-06-07 14:48:59 -0300

    Final

--- bloco_8/dia5/exercises/exercise2.js
+++ bloco_8/dia5/exercises/exercise2.js
@@ -8,8 +8,10 @@ const sum = (...args) => {
   return soma;
 }
 
-assert.strictEqual(sum(), 0);
-assert.strictEqual(sum(1), 1);
-assert.strictEqual(sum(1, 2), 3);
-assert.strictEqual(sum(1, 2, 3), 6);
-assert.strictEqual(sum(1, 2, 3, 4), 10);
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 1000));
+
+// assert.strictEqual(sum(), 0);
+// assert.strictEqual(sum(1), 1);
+// assert.strictEqual(sum(1, 2), 3);
+// assert.strictEqual(sum(1, 2, 3), 6);
+// assert.strictEqual(sum(1, 2, 3, 4), 10);
