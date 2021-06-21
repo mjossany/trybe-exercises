2a0e2833b6440c3b9fc73ae78c778fef2da606c8
Jossany Moura <mjossany@gmail.com>
2021-06-21 10:29:58 -0300

    Finaliza exercício 1 e 2

---
+++ Front/bloco11/dia1/exercises/exercise1/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+class App extends Component {
+  render() {
+    return (
+      <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul>
+    );
+  }
+}
+
+export default App;
--- README.md
+++
@@ -1,1 +0,0 @@
-Este é um repositório onde salvarei todos os exercícios que farei durante meu curso de formação Desenvolvedor Web Full Stack na Trybe!
