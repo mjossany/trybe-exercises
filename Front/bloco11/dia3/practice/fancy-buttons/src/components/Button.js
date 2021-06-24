fc38464490ebc1b83b344181a27aa8480c05a13c
Jossany Moura <mjossany@gmail.com>
2021-06-24 19:26:10 -0300

    Resolve última prática do dia

--- Front/bloco11/dia3/practice/fancy-buttons/src/components/Button.js
+++ Front/bloco11/dia3/practice/fancy-buttons/src/components/Button.js
@@ -1,22 +1,34 @@
 import React from 'react';
-
-
 class Button extends React.Component {
   constructor() {
     super()
-    this.handleClick = this.handleClick.bind(this)
+    this.countClick = this.countClick.bind(this)
+    this.paintButton = this.paintButton.bind(this)
     this.state = {
       numeroDeCliques: 0,
+      backgroundColor: 'white'
     }
   }
-  handleClick() {
+  countClick() {
+    // const { numeroDeCliques, backgroundColor} = this.state
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1
-    }))
+    }), () => {
+      this.paintButton();
+    })
   }
-  render() {
-    return (
-      <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
+    
+  paintButton () {
+    if (this.state.numeroDeCliques % 2 === 0 && this.state.numeroDeCliques !== 0 ) {
+    this.setState({backgroundColor: 'green'}, () => console.log(this.state.backgroundColor))
+    } else {
+    this.setState({backgroundColor: 'white'}, () => console.log(this.state.backgroundColor))
+    }
+  }
+
+    render() {
+      return (
+      <button onClick={this.countClick} style={{ background: this.state.backgroundColor }}>{this.state.numeroDeCliques}</button>
     ) 
   }
 }
