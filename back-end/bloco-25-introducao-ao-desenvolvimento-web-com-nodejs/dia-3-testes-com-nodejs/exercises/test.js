b03873821ffd4cb2cfdf7cde43ce8f8cbf093780
Jossany Moura <mjossany@gmail.com>
2021-10-23 17:42:00 -0300

    Finaliza exercícios 4 e 5

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/exercises/test.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/exercises/test.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const sinon = require('sinon');
 const { expect } = require('chai');
 const positivoNegativoNeutro = require('./exercise1');
+const escreveArquivo = require('./exercise4');
 
-describe('Testes do exercício 1', () => {
+const nomeDoArquivo = "testeDosTestes"
+const conteudoDoArquivo = "Eu vou me tornar bom em testes!"
+
+describe('Testes do exercício 1, 2 e 3', () => {
   describe('Quando receber um número maior que 0', () => {
     it('retorna uma string', () => {
       const retorno = positivoNegativoNeutro(1)
@@ -40,5 +46,27 @@ describe('Testes do exercício 1', () => {
       const retorno = positivoNegativoNeutro('quatro')
       expect(retorno).to.equal('o valor deve ser um número')
     })
+  })
+})
+
+describe('testes do exercício 4', () => {
+  describe('testes da função escreveArquivo', () => {
+
+    before(() => {
+      sinon.stub(fs, 'writeFileSync');
+    });
+    after(() => {
+      fs.writeFileSync.restore();
+    });
+
+    it('verifica se a função escreveArquivo retorna uma string', () => {
+      const retorna = escreveArquivo(nomeDoArquivo, conteudoDoArquivo)
+      expect(retorna).to.be.a('string')
+    })
+
+    it('verifica se a função retorna o seguinte valor', () => {
+      const retorna = escreveArquivo(nomeDoArquivo, conteudoDoArquivo);
+      expect(retorna).to.equal('ok');
+    });
   })
 })
