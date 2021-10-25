feb37502c6a7b263fbe69c6ad33821f3977609ef
Jossany Moura <mjossany@gmail.com>
2021-10-25 17:36:33 -0300

    Finaliza exercício 2

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/ping', handlePing = (_req, res) => {
+
+app.get('/ping', (_req, res) => {
   res.status(200).send('pong');
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ "message": `Hello, ${name}`})
+})
+
+app.use(function (err, req, res, next) {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
 });
 
 app.listen(3000, () => {
