def688ff150ed24e2e19bdb89a7281e4ac8d87fa
Jossany Moura <mjossany@gmail.com>
2021-10-25 19:31:57 -0300

    Finaliza exercício 7

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/simpsonsAPI.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/simpsonsAPI.js
@@ -10,6 +10,19 @@ app.get('/simpsons', async (req, res) => {
   res.status(200).send(result);
 });
 
+app.get('/simpsons/:id', async(req, res) => {
+  try {
+    const { id } = req.params;
+    const data = fs.readFileSync('./simpsons.json', 'utf8');
+    const result = JSON.parse(data);
+    const character = result.find((char) => char.id === id);
+    if (!character) res.status(404).json({"message": "simpson not found"});
+    res.status(200).json(character);
+  } catch(err) {
+    res.status(500).json({"message": err.message})
+  }
+});
+
 app.use(function (err, req, res, next) {
   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
 });
