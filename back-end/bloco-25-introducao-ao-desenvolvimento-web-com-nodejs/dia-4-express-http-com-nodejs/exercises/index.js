ca1b437af7891e8fd6446de59d7f89d144cbd71e
Jossany Moura <mjossany@gmail.com>
2021-10-25 17:46:26 -0300

    Finaliza exercício 3

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -11,13 +11,21 @@ app.get('/ping', (_req, res) => {
 
 app.post('/hello', (req, res) => {
   const { name } = req.body;
-  res.status(201).json({ "message": `Hello, ${name}`})
-})
+  res.status(201).json({ "message": `Hello, ${name}`});
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (parseInt(age) > 17) {
+    res.status(200).json({"message": `Hello, ${name}`});
+  };
+  res.status(401).json({"message": "Unauthorized"});
+});
 
 app.use(function (err, req, res, next) {
   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
 });
 
 app.listen(3000, () => {
-  console.log('Aplicação ouvindo na porta 3000')
+  console.log('Aplicação ouvindo na porta 3000');
 });
