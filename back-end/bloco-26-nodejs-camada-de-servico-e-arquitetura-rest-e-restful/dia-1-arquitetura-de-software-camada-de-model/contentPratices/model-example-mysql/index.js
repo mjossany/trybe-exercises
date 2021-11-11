d4c9c5a726ce9295352ac2c079e2aa9c5c1765c3
Jossany Moura <mjossany@gmail.com>
2021-11-11 17:39:07 -0300

    Finish model first practice with mysql

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/contentPratices/model-example-mysql/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/contentPratices/model-example-mysql/index.js
@@ -10,12 +10,16 @@ app.get('/authors', async (req, res) => {
 });
 app.get('/books', async (req, res) => {
   const { id } = req.query;
-
   const books = id 
   ? await Book.getByAuthorId(id)
   : await Book.getAll();
-
   res.status(200).json(books)
 });
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.getBookById(id);
+  if (!book) return res.status(404).json({ message: 'Not Found' });
+  return res.status(200).json(book);
+})
 
 app.listen(port, () => console.log(`Example app listen on port ${port}!`))
--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/contentPratices/model-example-mysql/models/Book.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/contentPratices/model-example-mysql/models/Book.js
@@ -1,17 +1,27 @@
 const connection = require('./connection');
 
+const getAll = async () => {
+  const [books] = await connection.execute('SELECT * FROM books');
+  return books;
+};
+
 const getByAuthorId = async (id) => {
   const query = 'SELECT * FROM books WHERE author_id = ?;';
   const [books] = await connection.execute(query, [id]);
   return books;
 }
 
-const getAll = async () => {
-  const [books] = await connection.execute('SELECT * FROM books');
-  return books;
-};
+const getBookById = async (id) => {
+  const query = 'SELECT * FROM books WHERE id = ?;';
+  const [book] = await connection.execute(query, [id]);
+
+  if (book.length === 0) return null;
+  
+  return book[0].title;
+}
 
 module.exports = {
   getAll,
   getByAuthorId,
+  getBookById,
 }
