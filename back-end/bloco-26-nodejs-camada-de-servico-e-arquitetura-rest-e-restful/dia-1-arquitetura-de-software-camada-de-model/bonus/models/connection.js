9db40cf6f07b1cdf4cbb0b62f8626145752d44e9
Jossany Moura <mjossany@gmail.com>
2021-11-13 12:04:23 -0300

    Restart bonus exercise

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/bonus/models/connection.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/bonus/models/connection.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2/promise');
+const { MongoClient } = require('mongodb');
 
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'M0urinh4',
-  database: 'users_crud',
-});
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGODB_URL = 'mongodb://127.0.0.1:27017';
+
+let db = null;
+
+const connection = () => {
+  return db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGODB_URL, OPTIONS)
+  .then((conn) => {
+    db = conn.db('model_example');
+    return db;
+  })
+};
 
 module.exports = connection;
