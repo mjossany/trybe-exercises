5156dc5a6302e960fae7fb29bd27ac90d0f55a53
Jossany Moura <mjossany@gmail.com>
2022-01-17 13:05:58 -0300

    Cria servidor, front e começa implementacao da lógica

---
+++ back-end/bloco-30-sockets/dia-4-praticando-socketio/leilao/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+require('./sockets/leilao')(io);
+
+http.listen(3001, () => {
+  console.log('Ouvindo na porta 3001');
+});
+
