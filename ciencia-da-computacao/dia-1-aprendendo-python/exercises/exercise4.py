35cd4c9e816e8c7420665b2a2d36d70d2c562581
Jossany Moura <mjossany@gmail.com>
2022-02-10 18:46:27 -0300

    Finaliza exercícios 2, 3, 4 e 5

---
+++ ciencia-da-computacao/dia-1-aprendendo-python/exercises/exercise4.py
@@ -0,0 +1,8 @@
+def find_biggest_name(names):
+    biggest_name = names[0]
+    for name in names:
+        if len(name) > len(biggest_name):
+            biggest_name = name
+    return biggest_name
+
+    find_biggest_name(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"])
---
+++ ciencia-da-computacao/dia-1-aprendendo-python/exercises/exercise5.py
@@ -0,0 +1,9 @@
+def paint_costs(area):
+    can_price = 80
+    required_liters = area / 3
+    required_cans = required_liters // 18
+    if required_cans % 18:
+        required_cans += 1
+    return required_cans, required_cans * can_price
+
+    paint_costs(150)
