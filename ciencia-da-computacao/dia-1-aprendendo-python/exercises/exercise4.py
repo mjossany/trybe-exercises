a7bfdad1744e3fee0b88fbf24f02ef2e28f0c69f
Jossany Moura <mjossany@gmail.com>
2022-02-11 17:13:22 -0300

    .

--- ciencia-da-computacao/dia-1-aprendendo-python/exercises/exercise4.py
+++ ciencia-da-computacao/dia-1-aprendendo-python/exercises/exercise4.py
@@ -3,6 +3,7 @@ def find_biggest_name(names):
     for name in names:
         if len(name) > len(biggest_name):
             biggest_name = name
-    return biggest_name
+    return print(biggest_name)
 
-    find_biggest_name(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"])
+
+find_biggest_name(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"])
--- ciencia-da-computacao/dia-1-aprendendo-python/exercises/exercise5.py
+++ ciencia-da-computacao/dia-1-aprendendo-python/exercises/exercise5.py
@@ -4,6 +4,7 @@ def paint_costs(area):
     required_cans = required_liters // 18
     if required_cans % 18:
         required_cans += 1
-    return required_cans, required_cans * can_price
+    return print(required_cans, required_cans * can_price)
 
-    paint_costs(150)
+
+paint_costs(150)
